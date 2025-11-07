db6d735caf593484ff786840f55bb2e25dca9e15
MohammadFaisal210 <faisalg.p.c.b.210@gmail.com>
2025-11-07 18:18:22 +0600

    Added collection dropdown with image preview

---
+++ assets/product-model.js
@@ -0,0 +1,49 @@
+class ProductModel extends HTMLElement {
+    constructor() {
+        super();
+        this.openModelModal();
+        this.addEventListener('click', this.loadContent);
+    }
+    getMediaID() {
+        const id = this.getAttribute('data-media-id');
+        return id;
+    }
+    getModal() {
+        const modal = document.getElementById("productModelModal");
+        return modal;
+    }
+    loadContent() {
+        shopify.loadFeatures(
+            [{
+                name: 'model-viewer-ui',
+                version: '1.0',
+                onLoad: this.setUpModelViewerUI.bind(this)
+
+            }]
+        )
+    }
+    setUpModelViewerUI(errors) {
+        if (errors) return;
+        this.setUpModelViewerUI = new Shopify.ModelViewerUI(document.querySelector('model-viewer'));
+    }
+    openModelModal() {
+        const mediaID = this.getMediaID();
+        const modal = this.getModal();
+
+
+        if (!mediaID) return;
+
+        const openModalButton = this.querySelector(`button[id="productModelOpenButton_${mediaID}"]`);
+
+        openModalButton.addEventListener('click', function(e) {
+            modal.querySelector("#body").innerHTML = "";
+            const template = document.querySelector(`product-model[data-media-id="${mediaID}"] > template`);
+
+            const clone = template.content.cloneNode(true);
+            modal.querySelector("#body").appendChild(clone);
+            modal.querySelector("#body > model-viewer").setAttribute("reveal", "auto");
+        });
+    }
+}
+
+const productModel = customElements.define('product-model', ProductModel);
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./**/*.{html,js,liquid}"],
+    theme: {
+        extend: {
+            height: {
+                '94': '22rem'
+            }
+        },
+    },
+    plugins: [],
+}
